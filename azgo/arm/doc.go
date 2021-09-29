7786f7c9fcde3e7e0c1083f140674b1455470c38
Aaron Wislang <aaron.wislang@microsoft.com>
2021-09-29 17:40:28 -0400

    Add package doc

---
+++ azgo/arm/doc.go
@@ -0,0 +1,6 @@
+/*
+Package arm is designed to provide examples of the Azure/azure-sdk-for go "track 2" packages.
+
+This begins with azidentity, azcore and armresources,
+and will be expanded to cover others soon.
+*/
